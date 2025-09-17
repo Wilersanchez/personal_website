92a28c7a27f37cd3016f25952245442570ed276c
Wiler Sanchez <Wilersanchez@ufl.edu>
2025-09-17 11:38:53 -0400

    Finished website template

--- my-portfolio-website/src/pages/Home.jsx
+++ my-portfolio-website/src/pages/Home.jsx
@@ -4,6 +4,9 @@ import { StarBackground } from '@/components/StarBackground';
 import { HeroSection } from "@/components/HeroSection";
 import { AboutSection } from "../components/AboutSection";
 import { SkillsSection } from "../components/SkillsSection";
+import { ProjectsSection } from "../components/ProjectsSection";
+import { ContactSection } from "../components/ContactSection";
+import { Footer } from "../components/Footer";
 
 export const Home = () => {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -19,8 +22,11 @@ export const Home = () => {
         <HeroSection />
         <AboutSection />
         <SkillsSection />
+        <ProjectsSection />
+        <ContactSection />
         </main>
 
         {/* Footer */}
+        <Footer />
     </div>;
 };
