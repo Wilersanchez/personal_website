db210ac36952a542936cf1ba3a408de6a40ad119
Wiler Sanchez <Wilersanchez@ufl.edu>
2025-09-17 11:48:22 -0400

    Let's see if it will work this time

--- my-portfolio-website/src/components/Footer.jsx
+++ my-portfolio-website/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { ArrowUp } from "lucide-react";
 
 export const Footer = () => {
     return (<footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
-        <p className=" text-sm test-muted-foreground">
+        <p className="text-sm test-muted-foreground">
             &copy; {new Date().getFullYear()} Wiler Sanchez, All rights reserved.
         </p>
 
